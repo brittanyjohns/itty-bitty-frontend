46b8967b996c39430a912adeef596851d92a29a7
Brittany Johns <bhannajohns@gmail.com>
2024-04-03 13:07:52 -0400

    its working

--- src/components/PredictiveImageGallery.tsx
+++ src/components/PredictiveImageGallery.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ImageComponent from "./ImageComponent";
-import { PredictiveImage } from "../data/images"; // Ensure this is the correct import
+import { PredictiveImage, getPredictiveImages } from "../data/images";
+import { get } from "react-hook-form";
+import { TextToSpeech } from "@capacitor-community/text-to-speech";
 
 interface PredictiveImageGalleryProps {
   initialImages: PredictiveImage[];
@@ -9,26 +11,62 @@ interface PredictiveImageGalleryProps {
 const PredictiveImageGallery: React.FC<PredictiveImageGalleryProps> = ({
   initialImages,
 }) => {
-  useEffect(() => {
-    // Preload images
-    initialImages.forEach((image) => {
-      image.nextImageIds.forEach((id) => {
-        const img = new Image();
-        img.src = image.src; // Assuming `src` is directly usable
-      });
+  const [currentImages, setCurrentImages] =
+    useState<PredictiveImage[]>(initialImages);
+  const inputRef = useRef<HTMLIonInputElement>(null);
+  const [showIcon, setShowIcon] = useState(false);
+
+  const handleImageClick = async (image: PredictiveImage) => {
+    const newImages = await getPredictiveImages(image.id);
+    console.log("newImages", newImages);
+    setCurrentImages(newImages);
+  };
+  const handleImageSpeak = (image: PredictiveImage) => {
+    const audioSrc = image.audio;
+    const label = image.label;
+    if (inputRef.current) {
+      inputRef.current.value += ` ${label}`;
+    }
+    if (inputRef.current?.value) {
+      setShowIcon(true);
+    } else {
+      setShowIcon(false);
+    }
+
+    if (!audioSrc) {
+      speak(label);
+      return;
+    }
+    // setAudioList([...audioList, audioSrc as string]);
+    const audio = new Audio(audioSrc);
+    audio.play();
+  };
+
+  const speak = async (text: string) => {
+    await TextToSpeech.speak({
+      text: text,
+      lang: "en-US",
+      rate: 1.0,
+      pitch: 1.0,
+      volume: 1.0,
+      category: "ambient",
     });
+  };
+
+  useEffect(() => {
+    setCurrentImages(initialImages);
   }, [initialImages]);
 
   return (
     <div className="grid grid-cols-4 gap-4">
-      {initialImages.map((image) => (
-        <ImageComponent
+      {currentImages.map((image, index) => (
+        <div
           key={image.id}
-          label={image.label}
-          src={image.src}
-          audioSrc={image.audio}
-          onClick={() => console.log(`Clicked on image ${image.id}`)}
-        />
+          onClick={() => handleImageClick(image)}
+          className="cursor-pointer"
+        >
+          <ImageComponent label={image.label} src={image.src} />
+        </div>
       ))}
     </div>
   );
--- src/pages/PredictiveIndex.tsx
+++ src/pages/PredictiveIndex.tsx
@@ -9,14 +9,12 @@ import {
   IonPage,
   IonRefresher,
   IonRefresherContent,
-  IonSearchbar,
   IonText,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import { PredictiveImage, getPredictiveImages } from "../data/images";
 import MainMenu from "../components/MainMenu";
-import SelectImageGallery from "../components/SelectImageGallery";
 import { useHistory } from "react-router";
 import Tabs from "../components/Tabs";
 import { addCircleOutline } from "ionicons/icons";
@@ -24,43 +22,29 @@ import { getInitialImages } from "../data/boards";
 import PredictiveImageGallery from "../components/PredictiveImageGallery";
 
 const ImagesScreen: React.FC = () => {
-  const [images, setImages] = useState<PredictiveImage[]>([]);
+  const [initialImages, setImages] = useState<PredictiveImage[]>([]);
   const history = useHistory();
   const [boardId, setBoardId] = useState("");
-  const fetchFirstBoard = async () => {
-    let imgs: PredictiveImage[] = [];
-    const board = await getInitialImages();
-    setBoardId(board.id);
-    console.log("setBoardId", board.id); // 'setBoardId', '1
-    if (board.id) {
-      imgs = await getPredictiveImages(board.id);
 
-      console.log("ImagesScreen - result", imgs);
-      setImages(imgs);
-    } else {
-      console.log("board.id not found");
-    }
-    return imgs;
+  const fetchFirstBoard = async () => {
+    const imgs = await getInitialImages();
+    setImages(imgs);
   };
 
   useEffect(() => {
-    async function fetchImages() {
-      const imgs = await fetchFirstBoard();
-      console.log("ImagesScreen - imgs", imgs);
-      setImages(imgs);
-    }
-    fetchImages();
+    // const fetchFirstBoard = async () => {
+    //   const board = await getInitialImages();
+    //   console.log("board", board);
+    //   const imgs = board;
+    //   setImages(imgs);
+    // };
+
+    fetchFirstBoard();
   }, []);
 
-  const handleImageClick = (image: PredictiveImage) => {
-    console.log("PredictiveImage Clicked", image);
-    // history.push(`/images/${image.id}`);
-  };
-
   const refresh = (e: CustomEvent) => {
-    setTimeout(() => {
-      e.detail.complete();
-    }, 3000);
+    // Implement the logic to refresh images here, for example:
+    e.detail.complete();
   };
 
   return (
@@ -70,25 +54,18 @@ const ImagesScreen: React.FC = () => {
         <IonHeader>
           <IonToolbar>
             <IonButtons slot="start">
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Image Gallery</IonTitle>
-            <IonButtons slot="end">
-              <IonButton routerLink="/images/add">
-                <IonIcon icon={addCircleOutline} />
-              </IonButton>
+              <IonMenuButton />
             </IonButtons>
           </IonToolbar>
-          <IonToolbar></IonToolbar>
         </IonHeader>
         <IonContent>
           <IonRefresher slot="fixed" onIonRefresh={refresh}>
-            <IonRefresherContent></IonRefresherContent>
+            <IonRefresherContent />
           </IonRefresher>
           <IonText>
-            <h1>Image Gallery {images.length}</h1>
+            <h1>Predictive Images - BETA {initialImages.length}</h1>
           </IonText>
-          {<PredictiveImageGallery initialImages={images} />}
+          <PredictiveImageGallery initialImages={initialImages} />
         </IonContent>
         <Tabs />
       </IonPage>
