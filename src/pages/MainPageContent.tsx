be71df243e72a8943724754c02115ac69afb2e2e
Brittany Johns <brittanyjohns@admins-MBP.lan>
2024-07-01 07:28:46 -0400

    css - i need to stop now

--- src/pages/MainPageContent.tsx
+++ src/pages/MainPageContent.tsx
@@ -10,9 +10,11 @@ import {
 import { useState } from "react";
 import { BetaRequest, createBetaRequest } from "../data/beta_requests";
 import { useHistory } from "react-router-dom";
+import "./MainPage.css";
 interface MainPageContentProps {
   ipAddr: string;
 }
+
 const MainPageContent = ({ ipAddr }: MainPageContentProps) => {
   const history = useHistory();
   const steps = [
@@ -92,21 +94,16 @@ const MainPageContent = ({ ipAddr }: MainPageContentProps) => {
   };
 
   return (
-    <div className="bg-black">
-      <div
-        className="hero_main1 bg-auto bg-center bg-no-repeat min-h-screen"
-        style={{
-          backgroundImage: `url(${getImageUrl("hero_main1", "webp")})`,
-        }}
-      >
-        <div className="flex flex-col justify-center items-center text-center py-8 bg-black bg-opacity-80  pb-15">
+    <div className="fixsed-bg">
+      <div className="">
+        <div className="flex flex-col justify-center items-center text-center py-4 lg:py-8  shadow-overlay">
           <h1 className="text-2xl md:text-5xl font-bold text-white mt-4">
             Empower Your Child's Communication
           </h1>
           <p className="mt-4 text-sm md:text-xl text-white">
             Discover the simplicity of SpeakAnyWay.
           </p>
-          <div className="container mx-auto px-1 mt-4">
+          {/* <div className="container mx-auto px-1 mt-4">
             <IonButton
               onClick={() => history.push("/about")}
               className=""
@@ -122,26 +119,41 @@ const MainPageContent = ({ ipAddr }: MainPageContentProps) => {
             >
               Demo
             </IonButton>
-          </div>
+          </div> */}
         </div>
 
-        <div className="container mx-auto px-2 mb-4">
-          <div className="my-5 text-center">
-            <section className="rounded-sm">
-              <div className="container mx-auto mb-4 p-2 shadow-lg w-2/3  bg-white rounded-lg shadow-lg">
-                <h2 className="text-2xl font-bold ">Join the Beta</h2>
-                <IonItem className="flex flex-col items-center mb-4">
+        <div className="flex flex-col justify-center items-center text-center gap-4">
+          <div className="fixed-bg">
+            <div className="h-10 md:h-24 lg:h-32 my-2 md:my-4">
+              <IonButton
+                onClick={() => history.push("/about")}
+                className=""
+                color="light"
+              >
+                Learn More
+              </IonButton>
+
+              <IonButton
+                onClick={() => history.push("/demo")}
+                className=""
+                color={"light"}
+              >
+                Demo
+              </IonButton>
+            </div>
+            <div className="relative h-80 md:h-96 lg:h-96 w-full">
+              <div className="p-2 bg-white absolute bottom-0 left-0 right-0 my-5">
+                <h2 className="text-2xl font-bold">Join the Beta</h2>
+                <IonItem className="w-2/3 mx-auto">
                   <IonInput
                     type="email"
                     value={email}
                     placeholder="Enter your email"
-                    // fill="solid"
                     onIonInput={(e) => setEmail(e.detail.value || "")} // Assuming Ionic React
                   />
 
                   <IonButton
                     className="mt-4"
-                    // size="large"
                     onClick={handleSubmitEmail}
                     disabled={!email}
                   >
@@ -160,26 +172,55 @@ const MainPageContent = ({ ipAddr }: MainPageContentProps) => {
                 <p className="text-center text-sm text-gray-600">
                   We're working on mobile apps for Android and iOS{" "}
                 </p>
-                <p className="mt-2 font-bold"> Stay tuned for updates!</p>
+                <p className="mt-2 font-bold">Stay tuned for updates!</p>
                 <div className="w-full">{/* Form component goes here */}</div>
               </div>
-            </section>
+            </div>
+            <div className="relative  h-80 md:h-96 lg:h-96">
+              <div className="shadow-overlay text-white px-5">
+                <section className="row-span-1 py-4">
+                  <div className="container mx-auto px-2">
+                    <h2 className="text-4xl font-bold text-center">Features</h2>
+                    <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-1 mt-4">
+                      {features.map((feature, index) => (
+                        <Link
+                          key={feature.id}
+                          to="/faq"
+                          className="bg-white rounded-lg shadow p-4 text-center"
+                        >
+                          <img
+                            src={getImageUrl(`feature_${index + 1}`, "webp")}
+                            alt={feature.title}
+                            className="h-40 object-cover rounded-md mx-auto"
+                          />
+                          <h3 className="mt-2 font-semibold text-lg text-purple-600">
+                            {feature.title}
+                          </h3>
+                          <p className="mt-2 text-sm text-gray-600">
+                            {feature.description}
+                          </p>
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
           </div>
 
-          <div className="mt-8 grid grid-cols-1 lg:grid-cols-1 gap-1 bg-black text-white bg-opacity-50">
+          {/* <div className="shadow-overlay text-white rounded-lg px-5">
             <section className="row-span-1 py-4">
               <div className="container mx-auto px-2">
-                <h2 className="text-4xl font-bold text-center ">Features</h2>
+                <h2 className="text-4xl font-bold text-center">Features</h2>
                 <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-1 mt-4">
                   {features.map((feature, index) => (
                     <Link
                       key={feature.id}
                       to="/faq"
-                      className="bg-white rounded-lg shadow py-4 text-center"
+                      className="bg-white rounded-lg shadow p-4 text-center"
                     >
                       <img
                         src={getImageUrl(`feature_${index + 1}`, "webp")}
-                        // src={`/src/assets/images/feature_${index + 1}.webp`}
                         alt={feature.title}
                         className="h-40 object-cover rounded-md mx-auto"
                       />
@@ -194,32 +235,7 @@ const MainPageContent = ({ ipAddr }: MainPageContentProps) => {
                 </div>
               </div>
             </section>
-            {/* <section className="row-span-1 py-4">
-              <div className="container mx-auto px-2">
-                <h2 className="text-4xl font-bold text-center mb-4">
-                  How It Works
-                </h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                  {steps.map((step, index) => (
-                    <div key={index} className="bg-white rounded-lg shadow p-4">
-                      <img
-                        // src={`src/assets/icons/round_itty_bitty_logo_1.png`}
-                        src={getIconUrl(`round_itty_bitty_logo_1`, "png")}
-                        alt={`Step ${index + 1}`}
-                        className="mx-auto h-20 w-20"
-                      />
-                      <h3 className="mt-4 font-semibold text-xl text-purple-600 text-center">
-                        Step {index + 1}: {step.title}
-                      </h3>
-                      <p className="mt-2 text-md text-gray-600 text-justify">
-                        {step.description}
-                      </p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </section> */}
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
