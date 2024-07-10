e48a12ca11261ee85460c4a850c995b7cf5a15c1
Brittany Johns <brittanyjohns@admins-MBP.lan>
2024-07-10 12:40:40 -0400

    mergey merge

--- src/pages/PricingPage.tsx
+++ src/pages/PricingPage.tsx
@@ -5,22 +5,13 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
-  IonButton,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardContent,
   IonBackButton,
   IonButtons,
 } from "@ionic/react";
 import MainMenu from "../components/main_menu/MainMenu";
-import { getImageUrl } from "../data/utils";
-import { useHistory } from "react-router";
-import PriceComparisonTable from "../components/utils/PricingTable";
 import PricingTable from "../components/utils/PricingTable";
 
 const PricingPage: React.FC = () => {
-  const history = useHistory();
   return (
     <>
       <MainMenu />
