f3d0d80f87b420658c85884f5f9830093ba55608
Brittany Johns <brittanyjohns@admins-MacBook-Pro.local>
2024-08-10 21:30:05 -0400

    admin

--- src/data/beta_requests.ts
+++ src/data/beta_requests.ts
@@ -1,29 +1,40 @@
 import { BASE_URL, userHeaders } from "./constants";
 interface RequestDetails {
-    client_ip: string;
+  client_ip: string;
 }
 export interface BetaRequest {
-    email: string;
-    details?: RequestDetails;
-    ip?: string;
-    created_at?: string;
+  email: string;
+  details?: RequestDetails;
+  ip?: string;
+  created_at?: string;
 }
 
-export const createBetaRequest = async (betaRequest: BetaRequest): Promise<BetaRequest> => {
-    const response = await fetch(`${BASE_URL}beta_requests`, {
-        method: 'POST',
-        headers: userHeaders,
-        body: JSON.stringify(betaRequest),
-    });
-    const newBetaRequest: BetaRequest = await response.json();
-    return newBetaRequest;
+export const createBetaRequest = async (
+  betaRequest: BetaRequest
+): Promise<BetaRequest> => {
+  const response = await fetch(`${BASE_URL}beta_requests`, {
+    method: "POST",
+    headers: userHeaders,
+    body: JSON.stringify(betaRequest),
+  });
+  const newBetaRequest: BetaRequest = await response.json();
+  return newBetaRequest;
 };
 
 export const fetchBetaRequests = () => {
-    const betaRequests = fetch(`${BASE_URL}beta_requests`, { headers: userHeaders })
-        .then(response => response.json())
-        .then(data => data)
-        .catch(error => console.error('Error fetching data: ', error));
+  const betaRequests = fetch(`${BASE_URL}beta_requests`, {
+    headers: userHeaders,
+  })
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.error("Error fetching data: ", error));
 
-    return betaRequests;
-}
+  return betaRequests;
+};
+
+export const getAllUsers = async () => {
+  const response = await fetch(`${BASE_URL}users`, { headers: userHeaders });
+  console.log("response", response);
+  const users = await response.json();
+  return users;
+};
--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -12,11 +12,18 @@ import {
 import MainMenu from "../components/main_menu/MainMenu";
 import Tabs from "../components/utils/Tabs";
 import { useCurrentUser } from "../hooks/useCurrentUser";
-import { BetaRequest, fetchBetaRequests } from "../data/beta_requests";
+import {
+  BetaRequest,
+  fetchBetaRequests,
+  getAllUsers,
+} from "../data/beta_requests";
 import { useEffect, useState } from "react";
+import StaticMenu from "../components/main_menu/StaticMenu";
+import MainHeader from "./MainHeader";
 const AdminDashboard: React.FC = () => {
-  const { isWideScreen, currentUser } = useCurrentUser();
+  const { isWideScreen, currentUser, currentAccount } = useCurrentUser();
   const [betaRequests, setBetaRequests] = useState<BetaRequest[]>([]);
+  const [users, setUsers] = useState<any[]>([]);
 
   const refresh = (e: CustomEvent) => {
     setTimeout(() => {
@@ -26,6 +33,7 @@ const AdminDashboard: React.FC = () => {
 
   useEffect(() => {
     loadBetaRequests();
+    loadAllUsers();
   }, []);
 
   const loadBetaRequests = async () => {
@@ -34,50 +42,85 @@ const AdminDashboard: React.FC = () => {
     console.log("Beta Requests: ", betaRequests);
   };
 
+  const loadAllUsers = async () => {
+    const users = await getAllUsers();
+    setUsers(users);
+    console.log("All Users: ", users);
+  };
+
   return (
     <>
-      <MainMenu />
+      <MainMenu
+        pageTitle="Dashboard"
+        isWideScreen={isWideScreen}
+        currentUser={currentUser}
+        currentAccount={currentAccount}
+      />
+      <StaticMenu
+        pageTitle="Dashboard"
+        isWideScreen={isWideScreen}
+        currentUser={currentUser}
+        currentAccount={currentAccount}
+      />
+
       <IonPage id="main-content">
-        <IonHeader className="bg-inherit shadow-none">
-          <IonToolbar>
-            <IonButtons slot="start">
-              {!isWideScreen && <IonMenuButton></IonMenuButton>}
-            </IonButtons>
-            <IonTitle>Admin Dashboard</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent>
+        <MainHeader
+          pageTitle="Dashboard"
+          isWideScreen={isWideScreen}
+          showMenuButton={!isWideScreen}
+        />
+        <IonContent className="ion-padding">
           <IonRefresher slot="fixed" onIonRefresh={refresh}>
             <IonRefresherContent></IonRefresherContent>
           </IonRefresher>
-          <div className="p-4">
-            <h1 className="text-4xl font-bold my-8"> Under Construction </h1>
-
-            <h1 className="text-2xl">Welcome to the Admin dashboard</h1>
+          <div className="ion-padding">
+            <h1 className="text-3xl">Welcome to the Admin dashboard</h1>
             <p className="text-lg">
               This is where you can view your account information, manage your
               subscriptions, and more.
             </p>
           </div>
-          {betaRequests.length > 0 && (
-            <div className="p-4 w-full md:w-1/2 mx-auto">
-              <h1 className="text-2xl">Beta Requests</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <div className="p-2 w-full">
+              {betaRequests.length > 0 && (
+                <div className=" w-full">
+                  <h1 className="text-2xl">Beta Requests</h1>
+                  <ul>
+                    {betaRequests.map((request, i) => (
+                      <li
+                        key={i}
+                        className="border-b border-gray-200 py-2 flex justify-between"
+                      >
+                        <span className="font-bold">{request.email}</span>
+                        <span className="text-right">
+                          {request.details?.client_ip}
+                        </span>
+                        <span className="text-xs block">
+                          {request.created_at}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+            <div className="p-1 w-full">
+              <h1 className="text-2xl">Users</h1>
               <ul>
-                {betaRequests.map((request, i) => (
+                {users.map((user, i) => (
                   <li
                     key={i}
                     className="border-b border-gray-200 py-2 flex justify-between"
                   >
-                    <span className="font-bold">{request.email}</span>
-                    <span className="text-right">
-                      {request.details?.client_ip}
-                    </span>
-                    <span className="text-xs block">{request.created_at}</span>
+                    <span className="font-bold">{user.id}</span>
+                    <span className="font-bold">{user.email}</span>
+                    <span className="text-right">{user.role}</span>
+                    <span className="text-xs block">{user.created_at}</span>
                   </li>
                 ))}
               </ul>
             </div>
-          )}
+          </div>
         </IonContent>
         {currentUser && <Tabs />}
       </IonPage>
